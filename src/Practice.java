1b6e4447aa3e31aea22c84e2ffc717a45070a75d
Kevin Minseok Oh <oms3749@gmail.com>
2021-10-30 00:07:37 +0900

    Add practice code of 'Data and operations'

--- src/Practice.java
+++ src/Practice.java
@@ -1,49 +1,44 @@
 
 public class Practice {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		System.out.println("\n--Numbers and Math--");
 		System.out.println(Math.PI);
-		System.out.println(Math.floor(Math.PI));		
+		System.out.println(Math.floor(Math.PI));
 		System.out.println(Math.ceil(Math.PI));
-		
-		
-		
+
 		System.out.println("\n--String and Char--");
-		System.out.println("Hello World"); //String
-		System.out.println('S'); //Character 작은 따옴표는 의미가 달라진다 
+		System.out.println("Hello World"); // String
+		System.out.println('S'); // Character 작은 따옴표는 의미가 달라진다
 		System.out.println("S"); // String
-		System.out.println("Hello "
-				+ "World");
-		System.out.println("Hello \nWorld"); //println에서도 \n이 적용됨 
+		System.out.println("Hello " + "World");
+		System.out.println("Hello \nWorld"); // println에서도 \n이 적용됨
 		System.out.println("Hello \"World\""); // '\'를 앞에 붙여줌으로써 Escape 함
-		
-		
-		
+
 		System.out.println("\n--Variables--");
-		//Variables
-		//데이터 타입을 정해주는 이유: 그 안에 있는 값은 반드시 문자, 숫자 등이다. (의심을 안 해도 됨)
-		
+		// Variables
+		// 데이터 타입을 정해주는 이유: 그 안에 있는 값은 반드시 문자, 숫자 등이다. (의심을 안 해도 됨)
+
 		int a = 49;
 		double b = 1.1;
 		String c = "Hello Minseok";
-		
-		
-		System.out.println(a); 
-		System.out.println(b);  
-		System.out.println(c);  
-		
-		
-		
-		System.out.println("--Using Var--"); //변수의 값이 바뀜으로써 수억개의 변수의 값을 한 번에 바꿀 수 있음 (값에 이름을 부여하는 것) 
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+		System.out.println("\n--Using Var--"); // 변수의 값이 바뀜으로써 수억개의 변수의 값을 한 번에 바꿀 수 있음 (값에 이름을 부여하는 것)
 		String name = "Minseok";
-		System.out.println("Hello "+name+" ... "+name+" ... "+name+ " ... bye.");
-		
-		
-		
-		
-		
-		
+		System.out.println("Hello " + name + " ... " + name + " ... " + name + " ... bye.");
+
+		System.out.println("\n--Casting--");
+		double e = 1;
+		System.out.println(e); // 1.0
+
+		int g = (int) 1.1; // 강제로(명시적으로) 자료형을 바꾼다.
+
+		String h = Integer.toString(g);
+		System.out.println(h);
+
 	}
-		
-	
+
 }
