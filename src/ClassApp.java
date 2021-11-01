9c8703d149b57c0c82e6d14ab190afa5d32337e2
Kevin Minseok Oh <oms3749@gmail.com>
2021-11-01 10:10:17 +0900

    Add ClassApp to understand concept of 'class'

---
+++ src/ClassApp.java
@@ -0,0 +1,11 @@
+
+public class ClassApp { // 클래스의 이해 및 활용
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI); // Math는 클래스 PI는 그 안에 있는 변수
+		System.out.println(Math.floor(1.6)); // 내림
+		System.out.println(Math.ceil(1.6)); // 올림
+
+		// 클래스를 만드는 법은 몰라도 사용하는 법은 알아야 한다
+	}
+}
